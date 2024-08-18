c2429043be4a3dea6f45e741bfaf2afb2e5c564e
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-08-18 20:00:06 +0600

    FoodPair Project Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '/logo.png'
 
 const Navbar = () => {
     return (
@@ -33,7 +34,9 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">
+                    <img className='w-32' src={logo} alt="" />
+                </a>
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
